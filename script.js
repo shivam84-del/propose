2e198136eb14ed442e2e04ac0a660b671ec19253
shivam kumar <shivam840700@gmail.com>
2025-12-04 14:23:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const codeInput = document.getElementById('code');
 const submitBtn = document.getElementById('submit-btn');
 const resultDiv = document.getElementById('result');
 
-const secretCode = 'Ayesha'; // Replace with your secret code
+const secretCode = 'Sanchita'; // Replace with your secret code
 const nextPageUrl = 'main.html'; // Replace with the URL of the new page
 
 form.addEventListener('submit', (e) => {
@@ -17,3 +17,4 @@ form.addEventListener('submit', (e) => {
 	}
 
 });
+
